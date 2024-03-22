e438fa7469a01e841281c16223f5c1431c5112cc
Davi Lacerda <davilacerda421@gmail.com>
2024-03-22 13:36:42 -0300

    Innitial commit

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text, Alert, TextInput, StyleSheet } from "react-native";
 
-export default function App() {
+const WelcomeMessage = () => {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    if (name.length === 10) {
+      Alert.alert("Aviso", "O nome atingiu 10 caracteres!");
+    }
+  }, [name]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="Digite seu nome"
+        onChangeText={(text) => setName(text)}
+        value={name}
+      />
+      <Text style={styles.welcomeMessage}>Seja bem-vindo, {name}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+    width: "80%",
+  },
+  welcomeMessage: {
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
+
+export default WelcomeMessage;
